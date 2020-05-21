ed008d78a2fa94619eb2022e48710e37e34b78c5
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 17:38:18 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,7 @@
 def count_strings(array)
-  
-  
-  
-  # Return the total number of strings in the provided array using the count enumerable
+  array.count do |element|
+    element.is_a?String
+    
 end
 
 
